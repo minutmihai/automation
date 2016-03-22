25d92f7abeeded14a955cc07f27c9d2451b62b99
Mihai Minut <minut.mihai@gmail.com>
2016-03-22 17:27:19 +0200

    remove URI from mongodb connection

--- HQ_automation/features/bootstrap/testMongoDB.php
+++ HQ_automation/features/bootstrap/testMongoDB.php
@@ -1,7 +1,8 @@
 <?php
 
 //connect do database
-$mongo = new MongoClient("mongodb://heroku_ftfzdmb9:48u7eotcssqek0i9sqq053dcdr@ds059215.mongolab.com:59215/heroku_ftfzdmb9");
+$URI = "";
+$mongo = new MongoClient($URI);
 
 //select db
 $db = $mongo->selectDB("heroku_ftfzdmb9");
